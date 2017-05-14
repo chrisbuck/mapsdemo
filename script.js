455a4663bddff89d0d4077e9750f2a64660bee48
chrisbuck <chrisbuck29@gmail.com>
2017-05-14 12:45:38 -0400

    authO to script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 (function(window, google, mwin){
-
+//User Authentication (AuthO)
+//lock
+var lock = new Auth0Lock('Sy3LfWdLUKI3FDD054pwhn1QOVroeBPg', 'chrisbuck.auth0.com');
+//implement login
+var btn_login = document.getElementById('btn-login');
+btn_login.addEventListener('click', function() {
+  lock.show();
+});
+//on authentication
+lock.on("authenticated", function(authResult) {
+  lock.getProfile(authResult.idToken, function(error, profile) {
+    if (error) {
+      // Handle error
+      return;
+    }
+    localStorage.setItem('id_token', authResult.idToken);
+    // Display user information
+    show_profile_info(profile);
+  });
+});
+    
 // ---- MAP PROPERTIES ---- //
 //element
 var mapEl = document.getElementById('map-canvas');
