4eae1e3a7cfa17050703560d4a7dbda0aa978a62
ChrisBuck <chrisbuck29@gmail.com>
2017-05-14 22:10:23 -0400

    resize profile icon

--- toolbar.js
+++ toolbar.js
@@ -156,7 +156,7 @@ var showUserProfile = function(profile) {
     $j('#profileIcon').css({
        width: 50,
         height: 50,
-        clipPath: 'circle(40px at center)'
+        clipPath: 'circle(25px at 47% 53%)'
     });
   /*
   document.getElementById('name').textContent = profile.name;
