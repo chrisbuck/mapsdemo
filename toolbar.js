18bea75baa0ba86fcdd59b1bbb445ad337eec3fb
ChrisBuck <chrisbuck29@gmail.com>
2017-05-14 21:22:18 -0400

    toggle visibility for form

--- toolbar.js
+++ toolbar.js
@@ -5,9 +5,11 @@ var $j = jQuery;
 var sBarCode = '<table id="settingsTbl"><tr><th colspan="2">Settings</th></tr>';
     sBarCode += '<tr><td>Elevation (heatmap):</td><td class="toggleCell"><div id="heatOuterToggle"><div id="heatInnerToggle"></div></div></td></tr>';
     sBarCode += '</table>';
+var profileCode = '<form class="form-signin"><h2 class="form-signin-heading">Please Log In</h2><input type="text" id="username" class="form-control" placeholder="Email address" autofocus required><input type="password" id="password" class="form-control" placeholder="Password" required><button class="btn btn-lg btn-default" type="button" id="btn-login">Log In</button><button class="btn btn-lg btn-primary" type="button" id="btn-register">Sign Up</button><button class="btn btn-lg btn-danger" type="button" id="btn-google">Log In with Google</button></form><div class="form-signin logged-in"><button class="bton btn-lg btn-default btn-block" type="button" id="btn-logout">Log out</button></div>';
 
 //Count
 var sBarCnt = 0;
+var userCnt = 0;
 var heatBool = false;
 
 //jQuery
@@ -41,6 +43,49 @@ $j('#settingsIcon').click(function(){
         $sBar.animate({width: 175}, 150);
     }
     sBarCnt++;
+    $j('#heatInnerToggle').click(function(){
+        var btn = $j(this);
+        var prnt = $j(this).parent();
+        if(heatBool == false){
+            btn.css({
+                position: 'relative',
+                left: 0
+            }).animate({
+                left: 18
+            },100, function(){
+                prnt.css('background-color','#279cff');
+            });
+            dotAddLoop();
+            heatBool = true;
+        } else {
+            btn.css({
+                position: 'relative',
+                left: 18
+            }).animate({
+                left: 0
+            },100, function(){
+                prnt.css('background-color','darkgray');
+            });
+            dotRemoveLoop();
+            heatBool = false;
+        }
+        });
+});
+$j('#profileIcon').click(function(){
+    $j(this).css('opacity', '1');
+    if (userCnt == 0){
+        $sBar.html(profileCode);
+        $sBar.animate({width: 175}, 150);
+    } else if ($sBar.width() == 175){
+        $sBar.animate({width: 0}, 100, function(){
+            $j(this).hide();
+        });
+    } else if ($sBar.width() == 0){
+        //$sBar.html(sBarCode);
+        $sBar.show();
+        $sBar.animate({width: 175}, 150);
+    }
+    userCnt++;
     $j('#heatInnerToggle').click(function(){
         var btn = $j(this);
         var prnt = $j(this).parent();
